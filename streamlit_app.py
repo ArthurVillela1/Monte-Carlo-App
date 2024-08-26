a9151a0a4a38b0f6279c7362a6d8788e383bf934
ArthurVillela <arthur.villela1@gmail.com>
2024-08-26 20:29:28 -0300

    new commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,17 @@ import seaborn as sn
 import plotly.graph_objs as go
 import matplotlib.pyplot as plt
 import math
+import pandas as pd
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 st.title("Monte Carlo Option Pricing")
 
 with st.sidebar:
-    vol = st.sidebar.slider("Volatility", 0.01, 1.00, 0.1)
-    t = st.sidebar.slider("Time to Maturity (months)", 1, 36, 12)
-    num_drifts = st.sidebar.slider("Time Steps", 1, 756, 252)
-    num_simulations = st.sidebar.slider("Number of simulations", 1, 50, 10)
+    sigma = st.sidebar.slider("Volatility", 0.01, 1.00, 0.1)
+    T = st.sidebar.slider("Time to Maturity (months)", 0, 36, 12)
+    drifts = st.sidebar.slider("Time Steps", 1, 756, 252)
+    simulations = st.sidebar.slider("Number of simulations", 1, 100, 10)
     S = st.sidebar.number_input("Current Asset Price (S)", value=50.00, step=0.01, min_value=0.0, max_value=9999.00, format="%.2f")
     K = st.sidebar.number_input("Strike (K)", value=70.00, step=0.01, min_value=0.0, max_value=9999.00, format="%.2f")
     rf = st.sidebar.number_input("Risk-Free Interest Rate (r)", value=0.10, step=0.01, min_value=0.0, max_value=9999.00, format="%.2f")
@@ -25,30 +27,30 @@ def option_payoff(option_type, S, K):
     elif option_type == "Put":
         return max(K - S, 0)
 
-def montecarlo_simulations(S, K, vol, rf, t, num_drifts, num_simulations):
+def montecarlo_simulations(s, k, vol, rf, t, num_drifts, num_simulations):
     
     dt = t/num_drifts
     prices = []
-    option_payoffs = []
-    
-    for i in range(num_simulations):
-        price = []
-        S*= math.exp((rf - 0.5 * vol**2) * dt + vol * math.sqrt(dt) * np.random.normal(0, 1))
-        price.append(S)
 
-    prices.append(price)
-    values = np.linspace(0, t, dt)
-    
-    for n in prices:
-        fig.add_trace(go.Scatter(x=values, y=prices, mode='lines', name=f'Path{n}'))
+    for i in range(num_simulations):
+        price_list = []
+        s_copy = s
+        for n in range(num_drifts):
+            s_copy*= math.exp((rf - 0.5 * vol**2) * dt + vol * math.sqrt(dt) * np.random.normal(0, 1))
+            price_list.append(s_copy)
+        prices.append(price_list)
 
     fig = go.Figure()
-    fig.update_layout(title=f'Simulated Price Paths',
-                      xaxis_title=time,
-                      yaxis_title='Price Paths')    
+    df = pd.DataFrame(prices).T
+    df.columns = [f"Simulation {i+1}" for i in range(num_simulations)]
 
+    fig = px.line(df, title="Simulated Price Paths")
+        
+    fig.update_layout(title=f'Simulated Price Paths',
+                      xaxis_title='Time',
+                      yaxis_title='Price') 
     return fig
 
-st.plotly_chart(montecarlo_simulations(K, vol, rf, t, num_drifts, num_simulations))
+st.plotly_chart(montecarlo_simulations(S, K, sigma, rf, T, drifts, simulations))
 
 
